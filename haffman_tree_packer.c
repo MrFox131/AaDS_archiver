2cbe807c965e61e582d8ca87ca5c50eefaa1ce83
Костюков Максим <enginer385@gmail.com>
2020-10-11 18:54:07 +1000

    Renamed some symboles due to codestyle

--- haffman_tree_packer.c
+++ haffman_tree_packer.c
@@ -11,28 +11,25 @@ struct node
 };
 #endif
 
-
-//TODO:NO RECURSION
-void haffman_tree_packer(struct node* tree, int *reallength, int *buffer_length, unsigned char* buffer){
-    // char *buffer = calloc(buffer_length, sizeof(char));
-    int initial_reallength = *reallength;
-    if (*buffer_length-*reallength<3){
+void haffman_tree_packer(struct node* tree, int *real_length, int *buffer_length, unsigned char* buffer){
+    int initial_real_length = *real_length;
+    if (*buffer_length-*real_length<3){
         *buffer_length+=1024;
         buffer = (char*)realloc(buffer, *buffer_length);
     }
     if (tree == NULL) {
-        buffer[*reallength] = '0';
-        (*reallength)++;
+        buffer[*real_length] = '0';
+        (*real_length)++;
     } else {
-        buffer[*reallength] = '1';
-        buffer[*reallength+1] = (*tree).letter;
-        *reallength+=2;
+        buffer[*real_length] = '1';
+        buffer[*real_length+1] = (*tree).letter;
+        *real_length+=2;
         if (!(*tree).is_letter){
-            haffman_tree_packer((*tree).left, reallength, buffer_length, buffer);
-            haffman_tree_packer((*tree).right, reallength, buffer_length, buffer);
+            haffman_tree_packer((*tree).left, real_length, buffer_length, buffer);
+            haffman_tree_packer((*tree).right, real_length, buffer_length, buffer);
         }
     }
-    if (initial_reallength==0){
-        buffer = realloc(buffer, (*reallength)*sizeof(char));
+    if (initial_real_length==0){
+        buffer = realloc(buffer, (*real_length)*sizeof(char));
     }
 }
