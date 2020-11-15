e3fcdcb7d5838c97670ddcb5ed7b94b5f4ef8b8d
Костюков Максим <enginer385@gmail.com>
2020-11-15 16:10:30 +1000

    First steps for archivate function

--- archiver.c
+++ archiver.c
@@ -3,6 +3,10 @@
 #include "haffman_tree_builder.h"     
 #include "haffman_tree_packer.h" 
 #include "stack.h" 
+#include "get_directory_structure.h"
+#include <string.h>
+
+#define STRUCTURE_SERVICE_BUFFER_MULTIPLIER 1024
 
 typedef struct node Node;
 
@@ -62,4 +66,33 @@ int haffman_archivate(FILE *in, FILE* out, int packed_tree_length, unsigned char
     fputc(filled_by==0?8:filled_by, out);
     cnt++;
     return packed_tree_length+cnt;
+}
+
+int archivate(char* name){
+    char* cleared_path = calloc(strlen(name)+1, sizeof(char));
+    strcpy(cleared_path, name);
+    if(cleared_path[strlen(cleared_path)-1]=='\\' || cleared_path[strlen(cleared_path)-1]=='/')
+        cleared_path[strlen(cleared_path)-1]='\0';
+    Directory* dir = get_directory_structure(cleared_path);
+    
+    
+    char* structure_service_buffer = calloc(STRUCTURE_SERVICE_BUFFER_MULTIPLIER, sizeof(char));
+    int structure_service_buffer_size = STRUCTURE_SERVICE_BUFFER_MULTIPLIER;
+    int current_carriage_pos=0;
+    while(structure_service_buffer_size-current_carriage_pos<strlen(dir->base)+1){
+        structure_service_buffer = realloc(structure_service_buffer, (structure_service_buffer_size+STRUCTURE_SERVICE_BUFFER_MULTIPLIER)*sizeof(char));
+        structure_service_buffer_size += STRUCTURE_SERVICE_BUFFER_MULTIPLIER;
+    }
+    strcpy(structure_service_buffer, dir->base);
+    printf("%d\n", strlen(dir->base));
+    current_carriage_pos+=strlen(dir->base)+1;
+
+    for(int i=0; i<dir->files_count; i++){
+        printf("%s\n", dir->files[i]);
+    }
+    
+    free(structure_service_buffer);
+    free(cleared_path);
+    destruct_dir(dir);
+    return 0;
 }
--- archiver.h
+++ archiver.h
@@ -3,11 +3,13 @@
 #include "haffman_tree_builder.h"     
 #include "haffman_tree_packer.h" 
 #include "stack.h" 
+#include "get_directory_structure.h"
 
 #ifndef ARCHIVER_H
 
 typedef struct node Node;
 int codes_generator(Node* root, unsigned char *codes[256], int *codes_length);
 int haffman_archivate(FILE *in, FILE* out, int packed_tree_length, unsigned char *packed_tree, Node* root);
+int archivate(char* name);
 
 #endif
