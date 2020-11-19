0ed677fa594b5e8fd8ba5c72d0d25b43ff648e4e
Костюков Максим <enginer385@gmail.com>
2020-11-19 18:36:31 +1000

    Added functions to verify that given path is able to be creted or is writable

---
+++ check_directory_properties.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <stdlib.h>
+#include <cstring>
+
+using namespace std;
+
+namespace fs = std::filesystem;
+
+bool check_directory_write_permission(char* name);
+
+extern "C" int check_directory_properties(char* name){
+    if(!fs::exists(name)){
+        if(!fs::create_directories(name)){
+            return -1; //unable to create directory
+        }
+        return 0;
+    }
+    if(!check_directory_write_permission(name)){
+        return -2; //unable to write to the directory
+    }
+}
+
+bool check_directory_write_permission(char *name){
+    char* name_ = (char*)malloc((strlen(name)+10)*sizeof(char));
+    strcpy(name_, name);
+    strcpy(name_+strlen(name), "/testfile");
+    FILE *testfile = fopen(name_, "w");
+    if(testfile ==NULL){
+        return false;
+    }
+    fclose(testfile);
+    remove(name_);
+    return true;
+}   
